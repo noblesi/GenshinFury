e4a8a2888f6f691306b25e96ae4a03dc8acb52d5
민성 <noblesi1570@gmail.com>
2024-06-21 00:41:54 +0900

    Update Asset교체

---
+++ Assets/Resources/Effect/Archer Pack/AP_Demo/AP_Scripts/AP_CreateProjectiles.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AP_CreateProjectiles : MonoBehaviour
+{
+    public Rigidbody Projectile;
+    public GameObject Launch;
+    public float Time = 1.0f;
+
+
+    private bool CreateInstances = true;
+    private Rigidbody Instance;
+    private GameObject Explosion;
+    private ParticleSystem Cast;
+
+    void Awake()
+    {
+        Cast = GetComponent<ParticleSystem>();
+        Cast.Stop();
+    }
+
+    private void OnEnable()
+    {
+        Cast.Stop();
+
+        if (Instance)
+        {
+            Destroy(Instance);
+        }
+
+        if (Explosion)
+        {
+            Destroy(Explosion);
+        }
+        CreateInstances = true;
+        StopCoroutine("ToCreate");
+        StopCoroutine("Create");
+        ToCreate();
+    }
+
+    void Update()
+    {
+        if (Instance == null && Explosion == null)
+        {
+            CreateInstances = true;
+            ToCreate();
+
+        }
+
+    }
+    void ToCreate()
+    {
+        StartCoroutine("Create");
+    }
+    public IEnumerator Create()
+    {
+        if (Cast.isStopped && CreateInstances)
+        {
+            Cast.Play();
+            yield return new WaitForSeconds(Time);
+
+            GameObject LaunchInstance;
+            LaunchInstance = Instantiate(Launch, transform.position, Launch.transform.rotation);
+            Destroy(LaunchInstance, 2.0f);
+
+            Instance = Instantiate(Projectile, transform.position, Projectile.transform.rotation);
+            CreateInstances = false;
+            Instance.transform.SetParent(this.transform);
+        }
+    }
+
+
+    public void GetExplosion(GameObject Exp)
+    {
+        Explosion = Exp;
+    }
+}
---
+++ Assets/Resources/Effect/Archer Pack/AP_Demo/AP_Scripts/AP_Demo.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AP_Demo : MonoBehaviour
+{
+    public GameObject[] ProjectilesList;
+
+    public Text Title;
+    public int Selection = 0;
+
+    public GameObject BackText;
+    public GameObject NextText;
+    public GameObject BackButton;
+    public GameObject NextButton;
+
+
+    void Start()
+    {
+        ProjectilesList[Selection].SetActive(true);
+        Title.text = "Prefab Name: " + ProjectilesList[Selection].gameObject.transform.name.ToString();
+    }
+    void Update()
+    {
+
+        if (Selection == 0)
+        {
+            BackText.SetActive(false);
+            BackButton.SetActive(false);
+        }
+        else
+        {
+            BackText.SetActive(true);
+            BackButton.SetActive(true);
+        }
+        if (Selection == ProjectilesList.Length - 1)
+        {
+            NextText.SetActive(false);
+            NextButton.SetActive(false);
+        }
+        else
+        {
+            NextText.SetActive(true);
+            NextButton.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Back();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Next();
+        }
+
+
+    }
+    public void Back()
+    {
+
+        if (Selection < ProjectilesList.Length && Selection != 0)
+        {
+            ProjectilesList[Selection].SetActive(false);
+            Selection -= 1;
+            ProjectilesList[Selection].SetActive(true);
+            Title.text = "Prefab Name: " + ProjectilesList[Selection].gameObject.transform.name.ToString();
+        }
+
+    }
+
+    public void Next()
+    {
+        if (Selection < ProjectilesList.Length && Selection != ProjectilesList.Length - 1)
+        {
+            ProjectilesList[Selection].SetActive(false);
+            Selection += 1;
+            ProjectilesList[Selection].SetActive(true);
+            Title.text = "Prefab Name: " + ProjectilesList[Selection].gameObject.transform.name.ToString();
+        }
+    }
+
+    public void Last()
+    {
+        if (Selection < ProjectilesList.Length)
+        {
+            ProjectilesList[Selection].SetActive(false);
+        }
+
+    }
+}
