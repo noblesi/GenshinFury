26c0ce5b4509aee142f3f6a3e9dfb1a0a3b6b802
민성 <noblesi1570@gmail.com>
2024-07-02 17:55:35 +0900

    Update 애니메이션 추가중

--- Assets/Scripts/Character/Enemy/Orc/OrcChieftain.cs
+++ Assets/Scripts/Character/Enemy/Orc/OrcChieftain.cs
@@ -2,35 +2,76 @@ using UnityEngine;
 
 public class OrcChieftain : OrcBase
 {
+    private int idleIndex = 0;
+
+    protected override void Start()
+    {
+        base.Start();
+        InvokeRepeating(nameof(SwitchIdleAnimation), 2f, 5f);
+    }
     protected override void InitializeAttackPatterns()
     {
-        attackPatterns.Add(() => BasicAttack());
-        attackPatterns.Add(() => PowerAttack());
-        attackPatterns.Add(() => ChargeAttack());
-        attackPatterns.Add(() => SlamAttack());
+        attackPatterns.Add(() => MeleeAttack());
+        attackPatterns.Add(() => ComboAttackV1());
+        attackPatterns.Add(() => ComboAttackV2());
+        attackPatterns.Add(() => ComboAttackV3());
     }
 
-    private void BasicAttack()
+    private void SwitchIdleAnimation()
     {
-        Debug.Log("Orc Chieftain performs a basic attack!");
+        if(currentState == MonsterState.Patrol)
+        {
+            idleIndex = 1 - idleIndex;
+            animator.SetFloat("IdleIndex", idleIndex);
+        }
+    }
+
+    protected override void UpdateAnimationState()
+    {
+        switch (currentState)
+        {
+            case MonsterState.Patrol:
+                animator.SetBool("isChasing", false);
+                animator.SetBool("isAttacking", false);
+                break;
+            case MonsterState.Chase:
+                animator.SetFloat("IdleIndex", 0f);
+                animator.SetBool("isChasing", true);
+                animator.SetBool("isAttacking", false);
+                break;
+            case MonsterState.Attack:
+                animator.SetFloat("IdleIndex", 0f);
+                animator.SetBool("isChasing", false);
+                animator.SetBool("isAttacking", true);
+                break;
+        }
+    }
+
+    private void MeleeAttack()
+    {
+        Debug.Log("Orc Chieftain performs a MeleeAttack!");
+        animator.SetTrigger("MeleeAttack");
         player.GetComponent<IDamageable>().TakeDamage(5, DamageType.Physical);
     }
 
-    private void PowerAttack()
+    private void ComboAttackV1()
     {
-        Debug.Log("Orc Chieftain performs a power attack!");
+        Debug.Log("Orc Chieftain performs a ComboAttack Version1");
+        animator.SetTrigger("ComboAttackV1");
         player.GetComponent<IDamageable>().TakeDamage(10, DamageType.Physical);
     }
 
-    private void ChargeAttack()
+    private void ComboAttackV2()
     {
-        Debug.Log("Orc Chieftain performs a charge attack!");
+        Debug.Log("Orc Chieftain performs a ComboAttack Version2!");
+        animator.SetTrigger("ComboAttackV2");
         player.GetComponent<IDamageable>().TakeDamage(12, DamageType.Physical);
     }
 
-    private void SlamAttack()
+    private void ComboAttackV3()
     {
-        Debug.Log("Orc Chieftain performs a slam attack!");
+        Debug.Log("Orc Chieftain performs a ComboAttack Version3!");
+        animator.SetTrigger("ComboAttackV3");
         player.GetComponent<IDamageable>().TakeDamage(20, DamageType.Physical);
     }
 }
